7eedcd3c6893f125be4b2494a07d2acf419b35db
npeyrouset <nils.peyrouset@ensae.fr>
2024-08-26 08:51:32 +0000

    add changes visualize templates

--- scripts/nils.py
+++ scripts/nils.py
@@ -47,9 +47,6 @@ learned_templates = get_data_driven_template_two_tasks(
 
                     train_task1, train_task2, B=n_train, seed=seed)
 print("La phase d'entrainement de Notip est terminée")
-#%%
-#Comparaison des kmin
-templates = {}
 
 df_tasks = pd.read_csv(os.path.join(script_path, 'contrast_list2.csv'))
 test_task1s, test_task2s = df_tasks['task1'], df_tasks['task2']
@@ -63,17 +60,124 @@ fmri_input, nifti_masker = get_processed_input(
                                                 smoothing_fwhm=smoothing_fwhm)
 stats_, p_values = stats.ttest_1samp(fmri_input, 0)
 p = fmri_input.shape[1]
-
-for k_min in [0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]:
-    learned_templates_kmin = learned_templates.copy()
-    learned_templates_kmin[:, :k_min] = np.zeros((n_train, k_min))
-    pval0, simes_tpl = calibrate_simes(fmri_input, alpha, k_max=p, B=B, n_jobs=n_jobs, seed=seed)
-    notip_tpl_kmin = calibrate_jer(alpha, learned_templates_kmin, pval0=pval0,
+#%%
+k_min = 0
+learned_templates_kmin = learned_templates.copy()
+learned_templates_kmin[:, :k_min] = np.zeros((n_train, k_min))
+pval0, simes_tpl = calibrate_simes(fmri_input, alpha, k_max=p, B=B, n_jobs=n_jobs, seed=seed)
+notip_tpl_kmin0 = calibrate_jer(alpha, learned_templates_kmin, pval0=pval0,
                                    k_max=k_max, k_min=k_min)
-    templates[k_min] = notip_tpl_kmin
-
-for k in list(templates.keys()):
-    plt.plot(templates[k], label=f'Notip + kmin = {k}') 
+_, region_notip_0 = sa.find_largest_region(p_values, notip_tpl_kmin0, TDP, nifti_masker)
+    
+plt.plot(notip_tpl_kmin0, label=f'Notip + kmin = {k_min}') 
+plt.legend()
+plt.show()
+#%%
+k_min = 10
+learned_templates_kmin = learned_templates.copy()
+learned_templates_kmin[:, :k_min] = np.zeros((n_train, k_min))
+pval0, simes_tpl = calibrate_simes(fmri_input, alpha, k_max=p, B=B, n_jobs=n_jobs, seed=seed)
+notip_tpl_kmin10 = calibrate_jer(alpha, learned_templates_kmin, pval0=pval0,
+                                   k_max=k_max, k_min=k_min)
+_, region_notip_10 = sa.find_largest_region(p_values, notip_tpl_kmin10, TDP, nifti_masker)
+    
+plt.plot(notip_tpl_kmin10, label=f'Notip + kmin = {k_min} /{region_notip_10} voxels') 
+plt.legend()
+plt.show()
+#%%
+k_min = 20
+learned_templates_kmin = learned_templates.copy()
+learned_templates_kmin[:, :k_min] = np.zeros((n_train, k_min))
+pval0, simes_tpl = calibrate_simes(fmri_input, alpha, k_max=p, B=B, n_jobs=n_jobs, seed=seed)
+notip_tpl_kmin20 = calibrate_jer(alpha, learned_templates_kmin, pval0=pval0,
+                                   k_max=k_max, k_min=k_min)
+_, region_notip_20 = sa.find_largest_region(p_values, notip_tpl_kmin20, TDP, nifti_masker)
+    
+plt.plot(notip_tpl_kmin20, label=f'Notip + kmin = {k_min} /{region_notip_20} voxels') 
+plt.legend()
+plt.show()
+#%%
+k_min = 27
+learned_templates_kmin = learned_templates.copy()
+learned_templates_kmin[:, :k_min] = np.zeros((n_train, k_min))
+pval0, simes_tpl = calibrate_simes(fmri_input, alpha, k_max=p, B=B, n_jobs=n_jobs, seed=seed)
+notip_tpl_kmin27 = calibrate_jer(alpha, learned_templates_kmin, pval0=pval0,
+                                   k_max=k_max, k_min=k_min)
+_, region_notip_27 = sa.find_largest_region(p_values, notip_tpl_kmin27, TDP, nifti_masker)
+    
+plt.plot(notip_tpl_kmin27, label=f'Notip + kmin = {k_min} /{region_notip_27} voxels') 
+plt.legend()
+plt.show()
+#%%
+k_min = 50
+learned_templates_kmin = learned_templates.copy()
+learned_templates_kmin[:, :k_min] = np.zeros((n_train, k_min))
+pval0, simes_tpl = calibrate_simes(fmri_input, alpha, k_max=p, B=B, n_jobs=n_jobs, seed=seed)
+notip_tpl_kmin50 = calibrate_jer(alpha, learned_templates_kmin, pval0=pval0,
+                                   k_max=k_max, k_min=k_min)
+_, region_notip_50 = sa.find_largest_region(p_values, notip_tpl_kmin50, TDP, nifti_masker)
+    
+plt.plot(notip_tpl_kmin50, label=f'Notip + kmin = {k_min} /{region_notip_50} voxels') 
+plt.legend()
+plt.show()
+#%%
+k_min = 100
+learned_templates_kmin = learned_templates.copy()
+learned_templates_kmin[:, :k_min] = np.zeros((n_train, k_min))
+pval0, simes_tpl = calibrate_simes(fmri_input, alpha, k_max=p, B=B, n_jobs=n_jobs, seed=seed)
+notip_tpl_kmin100 = calibrate_jer(alpha, learned_templates_kmin, pval0=pval0,
+                                   k_max=k_max, k_min=k_min)
+_, region_notip_100 = sa.find_largest_region(p_values, notip_tpl_kmin100, TDP, nifti_masker)
+    
+plt.plot(notip_tpl_kmin100, label=f'Notip + kmin = {k_min} /{region_notip_100} voxels') 
+plt.legend()
+plt.show()
+#%%
+k_min = 200
+learned_templates_kmin = learned_templates.copy()
+learned_templates_kmin[:, :k_min] = np.zeros((n_train, k_min))
+pval0, simes_tpl = calibrate_simes(fmri_input, alpha, k_max=p, B=B, n_jobs=n_jobs, seed=seed)
+notip_tpl_kmin200 = calibrate_jer(alpha, learned_templates_kmin, pval0=pval0,
+                                   k_max=k_max, k_min=k_min)
+_, region_notip_200 = sa.find_largest_region(p_values, notip_tpl_kmin200, TDP, nifti_masker)
+    
+plt.plot(notip_tpl_kmin200, label=f'Notip + kmin = {k_min} /{region_notip_200} voxels') 
+plt.legend()
+plt.show()
+#%%
+k_min = 500
+learned_templates_kmin = learned_templates.copy()
+learned_templates_kmin[:, :k_min] = np.zeros((n_train, k_min))
+pval0, simes_tpl = calibrate_simes(fmri_input, alpha, k_max=p, B=B, n_jobs=n_jobs, seed=seed)
+notip_tpl_kmin500 = calibrate_jer(alpha, learned_templates_kmin, pval0=pval0,
+                                   k_max=k_max, k_min=k_min)
+_, region_notip_500 = sa.find_largest_region(p_values, notip_tpl_kmin500, TDP, nifti_masker)
+    
+plt.plot(notip_tpl_kmin500, label=f'Notip + kmin = {k_min} /{region_notip_500} voxels') 
+plt.legend()
+plt.show()
+#%%
+k_min = 1000
+learned_templates_kmin = learned_templates.copy()
+learned_templates_kmin[:, :k_min] = np.zeros((n_train, k_min))
+pval0, simes_tpl = calibrate_simes(fmri_input, alpha, k_max=p, B=B, n_jobs=n_jobs, seed=seed)
+notip_tpl_kmin1000 = calibrate_jer(alpha, learned_templates_kmin, pval0=pval0,
+                                   k_max=k_max, k_min=k_min)
+_, region_notip_1000 = sa.find_largest_region(p_values, notip_tpl_kmin1000, TDP, nifti_masker)
+    
+plt.plot(notip_tpl_kmin1000, label=f'Notip + kmin = {k_min} /{region_notip_1000} voxels') 
+plt.legend()
+plt.show()
+#%%
+k_min = 2000
+learned_templates_kmin = learned_templates.copy()
+learned_templates_kmin[:, :k_min] = np.zeros((n_train, k_min))
+pval0, simes_tpl = calibrate_simes(fmri_input, alpha, k_max=p, B=B, n_jobs=n_jobs, seed=seed)
+notip_tpl_kmin2000 = calibrate_jer(alpha, learned_templates_kmin, pval0=pval0,
+                                   k_max=k_max, k_min=k_min)
+_, region_notip_2000 = sa.find_largest_region(p_values, notip_tpl_kmin2000, TDP, nifti_masker)
+    
+plt.plot(notip_tpl_kmin2000, label=f'Notip + kmin = {k_min} /{region_notip_2000} voxels') 
 plt.legend()
 plt.show()
 #%%
@@ -93,11 +197,14 @@ fmri_input, nifti_masker = get_processed_input(
 stats_, p_values = stats.ttest_1samp(fmri_input, 0)
 p = fmri_input.shape[1]
 pval0, calibrated_shifted_simes_tpl = calibrate_shifted_simes(fmri_input, alpha, B=B, n_jobs=n_jobs, seed=seed, k_min=k_min)
+_, region_calibrated_shifted_simes_tpl = sa.find_largest_region(p_values, calibrated_shifted_simes_tpl, TDP, nifti_masker)
 
 dicho_shifted_simes_tpl = calibrate_jer_param(alpha, generate_template=shifted_template_lambda, pval0=pval0, k_max=p, m=p, k_min=k_min, epsilon=0.0001)
+_, region_dicho_shifted_simes_tpl = sa.find_largest_region(p_values, dicho_shifted_simes_tpl, TDP, nifti_masker)
 
-plt.plot(calibrated_shifted_simes_tpl, label='pARI statistique pivotale')
-plt.plot(dicho_shifted_simes_tpl, label='pARI dichotomie paramétrique')
+
+plt.plot(calibrated_shifted_simes_tpl, label=f'pARI statistique pivotale /{region_calibrated_shifted_simes_tpl} voxels')
+plt.plot(dicho_shifted_simes_tpl, label=f'pARI dichotomie paramétrique /{region_dicho_shifted_simes_tpl} voxels')
 plt.legend()
 plt.show()
 #%%
@@ -118,11 +225,18 @@ fmri_input, nifti_masker = get_processed_input(
 stats_, p_values = stats.ttest_1samp(fmri_input, 0)
 p = fmri_input.shape[1]
 pval0, calibrated_simes_tpl = calibrate_simes(fmri_input, alpha, k_max=p, B=B, n_jobs=n_jobs, seed=seed)
+_, region_calibrated_simes_tpl = sa.find_largest_region(p_values, calibrated_simes_tpl, TDP, nifti_masker)
+
 
 truncated_simes_tpl1 = calibrate_jer_param(alpha, generate_template=linear_template_kmin, pval0=pval0, k_max=p, m=p, k_min=k_min, epsilon=0.001)
-pval0, truncated_simes_tpl2 = calibrate_truncated_simes(fmri_input, alpha, B=B, n_jobs=n_jobs, seed=seed, k_min=k_min)
+_, region_truncated_simes_tpl1 = sa.find_largest_region(p_values, truncated_simes_tpl1, TDP, nifti_masker)
 
-plt.plot(truncated_simes_tpl1, label='Calibrated Simes tronqué dochotomie paramétrique')
-plt.plot(truncated_simes_tpl2, label='Calibrated Simes tronqué statistique pivotale')
+pval0, truncated_simes_tpl2 = calibrate_truncated_simes(fmri_input, alpha, B=B, n_jobs=n_jobs, seed=seed, k_min=k_min)
+_, region_truncated_simes_tpl2 = sa.find_largest_region(p_values, truncated_simes_tpl2, TDP, nifti_masker)
+
+
+plt.plot(truncated_simes_tpl1, label=f'Calibrated Simes tronqué dochotomie paramétrique /{region_truncated_simes_tpl1} voxels')
+plt.plot(truncated_simes_tpl2, label=f'Calibrated Simes tronqué statistique pivotale /{region_truncated_simes_tpl2} voxels')
+plt.plot(calibrated_simes_tpl, label=f'Calibrated Simes /{region_calibrated_simes_tpl} voxels')
 plt.legend()
 plt.show()
