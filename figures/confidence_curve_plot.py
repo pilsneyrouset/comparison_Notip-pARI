f5d07132c531870047495524d97f84f03270e32a
pilsneyrouset <nils.peyrouset@ensae.fr>
2025-09-24 10:55:00 +0200

    code cleaning

--- figures/confidence_curve_plot.py
+++ figures/confidence_curve_plot.py
@@ -5,16 +5,11 @@ import sys
 import os
 from joblib import Memory
 from scipy import stats
-from tqdm import tqdm
-import sanssouci as sa
-import warnings
 from nilearn.datasets import fetch_neurovault
-from nilearn.maskers import NiftiMasker
 from nilearn._utils import check_niimg_3d
 from nilearn._utils.niimg import safe_get_data
 from scipy.stats import norm
-from nilearn.datasets import fetch_localizer_contrasts
-from matplotlib.ticker import FormatStrFormatter  # <-- ajout pour formater les ticks
+from matplotlib.ticker import FormatStrFormatter
 
 script_path = os.path.dirname(__file__)
 fig_path_ = os.path.abspath(os.path.join(script_path, os.pardir))
@@ -25,14 +20,9 @@ script_path = os.path.dirname(__file__)
 fig_path_ = os.path.abspath(os.path.join(script_path, os.pardir))
 fig_path = os.path.join(fig_path_, 'figures')
 
-from scripts.posthoc_fmri import compute_bounds, get_data_driven_template_two_tasks
-from sanssouci.lambda_calibration import calibrate_jer, calibrate_jer_param
-from scripts.posthoc_fmri import get_processed_input, ari_inference, calibrate_simes, calibrate_shifted_simes, calibrate_truncated_simes, _compute_hommel_value
-from sanssouci.reference_families import shifted_linear_template, linear_template_kmin
-from sanssouci.post_hoc_bounds import curve_min_tdp
+from scripts.posthoc_fmri import get_processed_input
 
-
-# Paramètres
+# Parameters
 seed = 42
 alpha = 0.05
 B = 10000
@@ -59,7 +49,8 @@ for i in range(len(test_task1s)):
     print("task number :", i)
     task1 = test_task1s[i]
     task2 = test_task2s[i]
-    print(task1, task2)
+    print("task1: ", task1)
+    print("task2: ", task2)
 
     fmri_input, nifti_masker = get_processed_input(task1, 
     task2, smoothing_fwhm=smoothing_fwhm)
@@ -76,7 +67,7 @@ for i in range(len(test_task1s)):
     for z in z_thresholds:
         count = np.sum(stat_map_ > z)
         voxel_counts[z] = count
-        print(f"Nombre de voxels avec z > {z} : {count}")
+        print(f"Number of voxels with z > {z} : {count}")
 
     TDP_ARI = np.load(f'task{i}/TDP_ARI_{alpha}.npy')
     TDP_Notip = np.load(f'task{i}/TDP_Notip_{alpha}.npy')
@@ -85,34 +76,34 @@ for i in range(len(test_task1s)):
     base_color = 'purple'
     alphas = np.linspace(0.3, 0.9, len(voxel_counts))
 
-    # --- Courbe TDP ---
+    # --- TDP Curve ---
     fig, ax = plt.subplots()
     ax.plot(TDP_ARI, label='ARI', color='red', alpha=0.5)
     ax.plot(TDP_Notip, label='Notip', color='green', alpha=0.5)
     ax.plot(TDP_pARI, label='pARI', color='blue', alpha=0.5)
-    for (z, count), seuil in zip(sorted(voxel_counts.items()), alphas):
-        ax.axvline(x=count, color=base_color, linestyle='--', alpha=seuil, label=f'z={z}')
+    for (z, count), thresh in zip(sorted(voxel_counts.items()), alphas):
+        ax.axvline(x=count, color=base_color, linestyle='--', alpha=thresh, label=f'z={z}')
     ax.set_xscale("log")
     ax.set_ylabel("Lower bound on True Discovery Proportion (TDP)")
     ax.set_xlabel("k")
     ax.grid(True, which="both", linestyle="--", linewidth=0.5)
     ax.legend()
-    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))  # <-- ici : 2 décimales
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
     plt.tight_layout()
     plt.savefig(f'task{i}/confidence_curve_TDP_{alpha}_full.pdf')
 
-    # --- Courbe FDP ---
+    # --- FDP Curve ---
     fig, ax = plt.subplots()
     ax.plot(FDP(TDP_ARI), label='ARI', color='red', alpha=0.5)
     ax.plot(FDP(TDP_Notip), label='Notip', color='green', alpha=0.5)
     ax.plot(FDP(TDP_pARI), label='pARI', color='blue', alpha=0.5)
-    for (z, count), seuil in zip(sorted(voxel_counts.items()), alphas):
-        ax.axvline(x=count, color=base_color, linestyle='--', alpha=seuil, label=f'z={z}')
+    for (z, count), thresh in zip(sorted(voxel_counts.items()), alphas):
+        ax.axvline(x=count, color=base_color, linestyle='--', alpha=thresh, label=f'z={z}')
     ax.set_xscale("log")
     ax.set_ylabel("Upper bound on False Discovery Proportion (FDP)")
     ax.set_xlabel("k")
     ax.grid(True, which="both", linestyle="--", linewidth=0.5)
     ax.legend()
-    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))  # <-- idem : 2 décimales
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
     plt.tight_layout()
     plt.savefig(f'task{i}/confidence_curve_FDP_{alpha}_full.pdf')
