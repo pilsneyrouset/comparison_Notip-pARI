0141192590867e8ee6bed9039d394c4b53758498
pilsneyrouset <nils.peyrouset@ensae.fr>
2025-12-08 14:12:03 +0100

    update import sanssouci

--- scripts/utils.py
+++ scripts/utils.py
@@ -21,9 +21,7 @@ from tqdm import tqdm
 from string import ascii_lowercase
 from scipy import ndimage
 import sys
-from sanssouci.lambda_calibration import get_pivotal_stats_shifted, calibrate_jer
-from sanssouci.reference_families import shifted_linear_template
-from sanssouci.post_hoc_bounds import min_tdp
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
@@ -238,10 +236,10 @@ def calibrate_shifted_simes(fmri_input, alpha, B=100, n_jobs=1, seed=None, k_min
                                                 n_jobs=n_jobs)
 
     # Compute pivotal stats and alpha-level quantile
-    piv_stat = get_pivotal_stats_shifted(pval0, k_min=k_min)
+    piv_stat = sa.get_pivotal_stats_shifted(pval0, k_min=k_min)
     lambda_quant = np.quantile(piv_stat, alpha)
     # Compute chosen template
-    shifted_simes_thr = shifted_linear_template(alpha=lambda_quant, k=p, m=p, k_min=k_min)
+    shifted_simes_thr = sa.shifted_linear_template(alpha=lambda_quant, k=p, m=p, k_min=k_min)
 
     return pval0, shifted_simes_thr
 
@@ -401,11 +399,10 @@ def get_clusters_table_with_TDP_task(stat_img, task_id, stat_threshold=3,
             masked_data_ = masked_data[masked_data != 0]
             # Compute TDP bounds on cluster using our 3 methods
             cluster_p_values = norm.sf(masked_data_)
-            ari_tdp = min_tdp(cluster_p_values, ari_thr)
-            simes_tdp = min_tdp(cluster_p_values, simes_thr)
-            notip_tdp = min_tdp(cluster_p_values, notip_thr)
+            ari_tdp = sa.min_tdp(cluster_p_values, ari_thr)
+            notip_tdp = sa.min_tdp(cluster_p_values, notip_thr)
             cluster_size_mm = int(np.sum(cluster_mask) * voxel_size)
-            pari_tdp = min_tdp(cluster_p_values, pari_thr)
+            pari_tdp = sa.min_tdp(cluster_p_values, pari_thr)
 
             # Get peaks, subpeaks and associated statistics
             subpeak_ijk, subpeak_vals = _local_max(
